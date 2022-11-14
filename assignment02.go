2cc130fad19ec9ccd247e85affa90d6005faa7a4
Abdul Manan <i190500@nu.edu.pk>
2022-11-14 22:13:34 +0500

    Blockchain Assignment02 Commit

---
+++ assignment02.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+type Transaction struct {
+	TransactionID string
+	Sender        string
+	Receiver      string
+	Amount        int
+}
+
+type Block struct {
+	Nonce int
+	BlockData        []Transaction
+	PrevPointer *Block
+	PrevHash    string
+	CurrentHash string
+	
+}
+
+type Blockchain struct {
+	ChainHead *Block
+}
+var flag=true
+func GenerateNonce(blockData []Transaction) int {
+	
+	//var onlyOnce sync.Once
+
+
+	    if(flag==true){
+
+			rand.Seed(time.Now().UnixNano())
+		    flag=false
+		}
+
+		
+		
+	
+	
+
+	
+
+
+	v := rand.Intn(100000)
+
+	return v
+
+
+}
+
+func CalculateHash(blockData []Transaction, nonce int) string {
+	dataString := ""
+	for i := 0; i < len(blockData); i++ {
+		dataString += (blockData[i].TransactionID + blockData[i].Sender +
+		blockData[i].Receiver + strconv.Itoa(blockData[i].Amount)) + strconv.Itoa(nonce)
+	}
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(dataString)))
+}
+
+func NewBlock(blockData []Transaction, chainHead *Block) *Block {
+
+
+	block:=new(Block)
+	block.BlockData=blockData
+    
+	block.Nonce=GenerateNonce(block.BlockData)
+
+	
+
+	if chainHead == nil {
+		block.PrevPointer = nil
+		block.PrevHash = "000000000000000 (Nil)" //Genesis Block so storing 0000000 as prev hash
+	}else 
+	 {
+       block.PrevPointer = chainHead
+	   block.PrevHash = chainHead.CurrentHash
+	}
+
+	block.CurrentHash=CalculateHash(blockData,block.Nonce)
+	
+	return block
+
+	
+	
+
+
+
+}
+
+func ListBlocks(chainHead *Block) {
+
+
+	i:=1
+	
+	for chainHead != nil {
+		
+		fmt.Println("\nBlock # ", i )
+		fmt.Println("_____________Data of this Block_______________:   ")
+		fmt.Println()
+		DisplayTransactions(chainHead.BlockData)
+		fmt.Println()
+		fmt.Println("Previous Pointer: ", &chainHead.PrevPointer)
+		fmt.Println("Current Block Nonce: ", chainHead.Nonce)
+		fmt.Println("Previous Hash:    ", chainHead.PrevHash)
+		fmt.Println("Current Hash: ", chainHead.CurrentHash)
+		chainHead = chainHead.PrevPointer
+		i++
+	  }
+	  fmt.Println()
+	  
+	 // DisplayTransactions(chainHead.BlockData)
+	
+}
+
+func DisplayTransactions(blockData []Transaction) {
+
+	for i:=0; i<len(blockData); i++{
+        fmt.Println("Transaction-------------------",i,"-----------------------")
+		
+		fmt.Println("Sender: ", blockData[i].Sender)
+		fmt.Println("Reciever: ", blockData[i].Receiver)
+		fmt.Println("Amount: ", blockData[i].Amount)
+
+	}
+	
+}
+
+func NewTransaction(sender string, receiver string, amount int) Transaction {
+
+	t1:=new(Transaction)
+
+	t1.Sender=sender
+	t1.Receiver=receiver
+	t1.Amount=amount
+
+	return *t1
+	
+	
+}
+
+
+func main() {
+	// Create blockchain
+	var blockchain Blockchain
+
+	// Create BlockData
+	var blockData []Transaction
+
+	// Create transactions # 1
+	transaction := NewTransaction("Satoshi", "Vitalik", 10)
+	// Add transaction to Block
+	blockData = append(blockData, transaction)
+
+	// Create transactions # 1
+	transaction = NewTransaction("Satoshi", "Cardano", 23)
+	// Add transaction to Block
+	blockData = append(blockData, transaction)
+
+	// Add block to blockchain
+	blockchain.ChainHead = NewBlock(blockData, nil)
+
+	// Create transactions # 3
+	transaction =NewTransaction("Alice", "Bob", 87)
+	// Add transaction to Block
+	blockData = append(blockData, transaction)
+
+	// Create transactions # 4
+	transaction = NewTransaction("Bob", "Alice", 10)
+	// Add transaction to Block
+	blockData = append(blockData, transaction)
+
+	// Add second block to blockchain
+	blockchain.ChainHead =NewBlock(blockData, blockchain.ChainHead)
+
+	
+	// Display blockchain
+	ListBlocks(blockchain.ChainHead)
+
+	// Verify cheating
+	for blockchain.ChainHead != nil {
+		if blockchain.ChainHead.CurrentHash != CalculateHash(blockchain.ChainHead.BlockData, blockchain.ChainHead.Nonce) {
+			fmt.Println("Verification failed!")
+			return
+		}
+		blockchain.ChainHead = blockchain.ChainHead.PrevPointer
+	}
+	fmt.Println("Verification passed!")
+}
+
